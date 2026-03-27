b4054df35d71e27a3f799d8314a3a0c0a2464416
irfan <irfan.hamzic2003@gmail.com>
2026-03-27 22:21:33 +0100

    updated

--- incident-service/src/main/java/com/hamzic/ihub/incident_service/dto/CreateIncidentRequest.java
+++ incident-service/src/main/java/com/hamzic/ihub/incident_service/dto/CreateIncidentRequest.java
@@ -1,4 +1,51 @@
 package com.hamzic.ihub.incident_service.dto;
 
+import com.hamzic.ihub.incident_service.entity.IncidentStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class CreateIncidentRequest {
+
+    @NotBlank
+    private String title;
+    private String description;
+
+    @NotNull
+    private IncidentStatus status;
+    private Long assignedUserId;
+
+    public CreateIncidentRequest() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public IncidentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(IncidentStatus status) {
+        this.status = status;
+    }
+
+    public Long getAssignedUserId() {
+        return assignedUserId;
+    }
+
+    public void setAssignedUserId(Long assignedUserId) {
+        this.assignedUserId = assignedUserId;
+    }
 }
--- incident-service/src/main/java/com/hamzic/ihub/incident_service/repository/IncidentRepository.java
+++ incident-service/src/main/java/com/hamzic/ihub/incident_service/repository/IncidentRepository.java
@@ -1,4 +1,11 @@
 package com.hamzic.ihub.incident_service.repository;
 
-public class IncidentRepository {
+import com.hamzic.ihub.incident_service.entity.Incident;
+import com.hamzic.ihub.incident_service.entity.IncidentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IncidentRepository extends JpaRepository<Incident,Long> {
+    List<Incident> findByStatus(IncidentStatus status);
 }
